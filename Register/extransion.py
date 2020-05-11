1b1c632ff2dc657aa385059e24deb74f17fa23bc
Zeerti <skarrj@gmail.com>
2020-05-11 08:10:13 -0600

    Updated Recorder class with mouse/keyboard handling

--- Register/extransion.py
+++ Register/extransion.py
@@ -1,3 +1,18 @@
-if __name__ == "__main__":
-    # Start doing stuff
-    pass
+import eel
+
+
+def main():
+    try:
+        print('Starting EEL GUI...')
+        eel.init('web')
+        eel.start('index.html', port=8000, mode='edge', cmdline_args=['--start-fullscreen'])
+    except EnvironmentError:
+        print("\n****************\nFailed auto-launching edge. Attemping to load default browswer\n****************\n\nPlease navigate to localhost:8000/index.html manually if it doesn't automatically open")
+        # If Chrome isn't found, fallback to Microsoft Edge on Win10 or greater
+        try:
+            eel.start('index.html', port=8000, mode=False)
+        except:
+            raise
+
+if __name__ == '__main__':
+    main()
--- Register/recorder.py
+++ Register/recorder.py
@@ -1,18 +1,42 @@
-import pywinauto
+from pynput import mouse 
+from pynput import keyboard
+# from dbHandler import saveToDatabase
 
 class Recorder(): 
     def __init__(self):
-        actions = []
-    
-    def getMouseCoords(self):
-        _x = 0
-        _y = 0
-        return x,y
-    
-    def appendToActions(self, x, y):
-        pass
-    
+        self.actions = []
+        self.listenFlag = True
+        self.mouseListener = mouse.Listener(on_click=self.appendToActions)
+        self.mouseListener.start()
 
-
-
+        self.keyboardListener = keyboard.Listener(on_press=self.toggleListening)
+        self.keyboardListener.start()
     
+    def appendToActions(self, x, y, button, pressed):
+        if(not pressed and self.listenFlag == True):
+            print(x, y, button, pressed)
+            _x = x
+            _y = y
+            _button = button
+
+            tempDict = { 
+                'x': _x,
+                'y': _y,
+                'button': _button
+            }
+
+            self.actions.append(tempDict)
+        
+    # Current design will force program to be restarted if listeners are stopped
+    # need to add in a flag to have it just ignore the listener 
+    def toggleListening(self, key):
+        if(not self.listenFlag){
+            # TODO: YELL THAT YOU ARE ABOUT TO RE-ENABLE RECORDING
+        }
+        if (key == Key.esc){
+            self.listenFlag = not self.listenFlag
+        }
+    
+rec = Recorder() 
+while True:
+    a = 7
