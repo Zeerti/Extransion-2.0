f7396d8c93f43de7737d7e85162c526fee9ab54e
Tucker Warlock <tuckerpw01123@gmail.com>
2020-05-10 21:13:48 -0600

    Update dbHandler.py
    Created deleteDatabase functions. Moves current db files to newly created backup folder.

--- Register/dbHandler.py
+++ Register/dbHandler.py
@@ -1,4 +1,6 @@
 import shelve
+import os
+import shutil
 
 # Returns the current db file AS A COPY
 # You will need to explicityly resave it with the saveToDatabase function
@@ -32,6 +34,39 @@ def saveToDatabase(db, actions):
     db['actions'] = actions
     db.close()
 
-# Deletes existing db files
+# Moves existing db files to backup folder
 def deleteDatabase():
     # TODO: Delete DB files
+    
+    # Directory to be created
+    directory = 'databaseBackup'
+    
+    # Parent Directory path 
+    parent_dir = cwd = os.getcwd()
+    
+    # Path 
+    path = os.path.join(parent_dir, directory) 
+    
+    # Create the directory 
+    try: 
+        os.mkdir(path) 
+        print("Directory '%s' created" %directory) 
+
+        # if directory / file that is to be created already exists then 'FileExistsError' will be raised by os.mkdir() method 
+        # Similarly, if the specified path is invalid 'FileNotFoundError' Error will be raised 
+
+    except OSError as error: 
+        print(error)
+
+    # Define files to be moved
+    sourceFile1 = 'pickle.db.dat'
+    sourceFile2 = 'pickle.db.bak'
+    sourceFile3 = 'pickle.db.dir'
+    destDir =  directory
+
+    # Move database files from the Extransion directory to newly created databaseBackup folder
+    shutil.move(sourceFile1, destDir)
+    shutil.move(sourceFile2, destDir)
+    shutil.move(sourceFile3, destDir)
+
+# deleteDatabase()
